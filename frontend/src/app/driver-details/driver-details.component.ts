f1e777d890dd98bac204f23f7fc8eb27fb1e0094
ayumi <brianmaysebastian@gmail.com>
2024-05-18 11:45:52 +0800

    add driver and trackinglog details components

--- frontend/src/app/driver-details/driver-details.component.ts
+++ frontend/src/app/driver-details/driver-details.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Driver } from '../driver';
+import { DriverService } from '../driver.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-driver-details',
   templateUrl: './driver-details.component.html',
-  styleUrls: ['./driver-details.component.css']
+  styleUrls: ['./driver-details.component.css'],
 })
 export class DriverDetailsComponent {
+  driverID: number;
+  driver!: Driver;
 
+  constructor(
+    private route: ActivatedRoute,
+    private driverService: DriverService
+  ) {
+    this.driverID = 0;
+  }
+
+  ngOnInit(): void {
+    this.driverID = this.route.snapshot.params['driverID'];
+
+    this.driver = new Driver();
+    this.driverService.getDriverById(this.driverID).subscribe((data) => {
+      this.driver = data;
+    });
+  }
 }
--- frontend/src/app/trackinglog-details/trackinglog-details.component.ts
+++ frontend/src/app/trackinglog-details/trackinglog-details.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Trackinglog } from '../trackinglog';
+import { TrackinglogService } from '../trackinglog.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-trackinglog-details',
   templateUrl: './trackinglog-details.component.html',
-  styleUrls: ['./trackinglog-details.component.css']
+  styleUrls: ['./trackinglog-details.component.css'],
 })
 export class TrackinglogDetailsComponent {
-  
+  trackingID: number;
+  trackinglog!: Trackinglog;
+
+  constructor(
+    private route: ActivatedRoute,
+    private trackinglogService: TrackinglogService
+  ) {
+    this.trackingID = 0;
+  }
+
+  ngOnInit(): void {
+    this.trackingID = this.route.snapshot.params['trackingID'];
+
+    this.trackinglog = new Trackinglog();
+    this.trackinglogService.getTrackinglogById(this.trackingID).subscribe((data) => {
+      this.trackinglog = data;
+    });
+  }
 }
